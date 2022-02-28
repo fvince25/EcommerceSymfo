cf023ac2a991f886e9d1bb75d6ed312d5b3bfae3
Vincent Fritz <fvince25@yahoo.fr>
2022-02-28 09:23:27 +0100

    eventslisteners sur les forms commenté.

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -45,9 +47,46 @@ class ProductType extends AbstractType
                 'placeholder' => '-- Choisir une catégorie --',
                 'class' => Category::class,
                 'choice_label' => 'name'
-
             ]);
-        ;
+
+
+        // Exemple d'utilisation des eventlisters sur les forms :
+        // C'est à titre d'exemple : L'utilisation dans le cas de transformation de données est dévoyée.
+
+//        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+//            $product = $event->getData();
+//        });
+//
+//        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+//            $product = $event->getData();
+//        });
+
+        // $builder->addEventListener('form.pre_set_data');
+        // EN dur ce n'est pas conseillé : Si le nom change d'une version de symfony à l'autre on sera embêté.
+
+        // Exemple d'addeventlistener (EXEMPLE si on ne veut pas ajouter category si on est en edit)
+
+//        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+//
+//            $form = $event->getForm();
+//            /**
+//             * @var Product
+//             */
+//            $product = $event->getData();
+//            // Normalement avec un bon éditeur,
+//            // ce qui n'est pas le cas ici, on devrait avoir la connaissance des méthodes de products
+//
+//            if($product->getId() === null) {
+//                $form->add('category', EntityType::class, [
+//                    'label' => 'Catégorie',
+//                    'attr' => [],
+//                    'placeholder' => '-- Choisir une catégorie --',
+//                    'class' => Category::class,
+//                    'choice_label' => 'name'
+//                ]);
+//            }
+//        });
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
