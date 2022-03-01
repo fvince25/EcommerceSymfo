c7a6c6d41f4474021bc06d7455c3f2a61b950b96
Vincent Fritz <fvince25@yahoo.fr>
2022-03-01 15:12:26 +0100

    Retour à guard/authenticator . Suppression de form_login. Activation des rôles dans access_control (security.yaml)

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,7 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        // todo
+        return new RedirectResponse('/login');
     }
 
     public function supportsRememberMe()
