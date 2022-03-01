2c7f1320ed610a7216a2bc90e948a11feb7f8a01
Vincent Fritz <fvince25@yahoo.fr>
2022-03-01 17:36:55 +0100

    Accès spécifique d'un utilisateur pour une catégorie précise avec voter façon programmatique

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -84,15 +84,17 @@ class CategoryController extends AbstractController
             throw new NotFoundHttpException("Cette catégorie n'existe pas");
         }
 
-        $user = $this->getUser();
+        $this->denyAccessUnlessGranted('CAN_EDIT', $category, "Vous n'êtes pas le propriétaire de cette catégorie");
 
-        if(!$user) {
-            return $this->redirectToRoute("security_login");
-        }
-
-        if ($user !== $category->getOwner()) {
-           throw new AccessDeniedHttpException("Vous n'êtes pas proprio de cette catégorie");
-        }
+//        $user = $this->getUser();
+//
+//        if(!$user) {
+//            return $this->redirectToRoute("security_login");
+//        }
+//
+//        if ($user !== $category->getOwner()) {
+//           throw new AccessDeniedHttpException("Vous n'êtes pas proprio de cette catégorie");
+//        }
 
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
