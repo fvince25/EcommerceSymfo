31f5830b26ad36c5d6c71837a44205c60a205320
Vincent Fritz <fvince25@yahoo.fr>
2022-03-03 10:47:32 +0100

    Ajout cart plus flashbag détaillé. détail retiré ensuite

---
+++ src/Controller/CartController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
+     */
+    public function add($id, ProductRepository $productRepository, SessionInterface $session): Response
+    {
+//        $cart = $request->getSession()->get('cart', []);
+        $cart = $session->get('cart', []);
+
+        $product = $productRepository->find($id);
+
+        if(!$product) {
+            throw $this->createNotFoundException("Le produit $id n'existe pas !");
+        }
+
+        if (array_key_exists($id, $cart)) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+//        $request->getSession()->set('cart', $cart);
+        $session->set('cart', $cart);
+
+        /**
+         * @var FlashBag
+         */
+        $flashBag = $session->getBag('flashes');
+
+        // Ajoute des messages à la pile
+        $flashBag->add('success',"Le produit a bien été ajouté au panier");
+        $flashBag->add('info',"info 1");
+        $flashBag->add('info',"info 2");
+        $flashBag->add('warning',"warning");
+
+        // Lit le message et dépile le message comme un répondeur. ça fait remove
+//        dump($flashBag->get('warning'));
+
+//        dd($session->getBag('flashes'));
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug()
+        ]);
+
+        //        $request->getSession()->remove('cart');
+
+//        return $this->render('cart/add.html.twig', [
+//            'controller_name' => 'CartController',
+//        ]);
+    }
+}
