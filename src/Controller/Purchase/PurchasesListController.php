2acf0837093dd3c17d37f08798ad2146b3a8a909
Vincent Fritz <fvince25@yahoo.fr>
2022-03-05 09:08:40 +0100

    Suppression de la révision sur l'injection de dépendance. Utilisation de l'abstractcontroller dans PurchasesListController.php

--- src/Controller/Purchase/PurchasesListController.php
+++ src/Controller/Purchase/PurchasesListController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -17,43 +18,19 @@ use Twig\Environment;
 
 class PurchasesListController extends AbstractController {
 
-    protected $security;
-    protected $router;
-    protected $twig;
-
-    public function __construct(Security $security, RouterInterface $router, Environment $twig) {
-        $this->security = $security;
-        $this->router = $router;
-        $this->twig = $twig;
-
-    }
-
-
     /**
      * @Route("/purchases", name="purchase_index")
+     * @IsGranted("ROLE_USER", message="Vous devez être connecté pour accéder à vos commandes")
      */
     public function index() {
 
         // Surcharge classe User
         /** @var User */
-        $user = $this->security->getUser();
+        $user = $this->getUser();
 
-        // Par défaut c'est un userinterface et pas un user !!!!
-
-        if(!$user) {
-            // On aurait pu utiliser UrlGeneratorInterface
-//            $url = $this->router->generate('homepage');
-//            return new RedirectResponse($url);
-            // Exception super pratique qui renvoie autmatiquement à la page login, et qui remet sur la page en cours une fois connecté !!!
-
-            throw new AccessDeniedException("Vous devez être connecté pour accéder à vos commandes");
-        }
-
-        $html = $this->twig->render('purchase/index.html.twig', [
+        return $this->render('purchase/index.html.twig', [
             'purchases' => $user->getPurchases()
         ]);
-
-        return new Response($html);
 
     }
 
