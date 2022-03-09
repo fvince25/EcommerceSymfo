cd2be2ceb511d00d1e0d8d492a4181edc5571b2f
Vincent Fritz <fvince25@yahoo.fr>
2022-03-09 14:12:07 +0100

    Composant Email / écriture d'un Email simple --------------------------------------------------

--- src/EventDispatcher/ProductViewSubscriber.php
+++ src/EventDispatcher/ProductViewSubscriber.php
@@ -5,15 +5,20 @@ namespace App\EventDispatcher;
 use App\Event\ProductViewEvent;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 
 class ProductViewSubscriber implements EventSubscriberInterface
 {
 
     protected $logger;
+    protected $mailer;
 
-    public function __construct(LoggerInterface $logger) {
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer) {
         $this->logger = $logger;
+        $this->mailer = $mailer;
     }
     /**
      * @inheritDoc
@@ -27,6 +32,15 @@ class ProductViewSubscriber implements EventSubscriberInterface
 
 
     public function sendViewProductEmail(ProductViewEvent $productViewEvent) {
+
+        $email = new Email();
+        $email->from(new Address("contact@mail.com", "Infos de la boutique"))
+            ->to("admin@mail.com")
+            ->text("Un visiteur est en train de voir la page du produit n°".$productViewEvent->getProduct()->getId())
+            ->subject("Visite du produit n°".$productViewEvent->getProduct()->getId());
+
+        $this->mailer->send($email);
+
         $this->logger->info("Email envoyé pour la visualisation du produit ".$productViewEvent->getProduct()->getName());
     }
 
